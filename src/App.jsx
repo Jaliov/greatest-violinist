3e44a84e0948880504e627bb683b3590dfe19223
Joel Rudin <jarud55@gmail.com>
2023-10-03 11:06:00 -0400

    greatest-violinist

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Bio from "./Bio";
+
+function App() {
+ 
+
+  return (
+    <div className="dum">
+      <Bio name="Jascha Heifetz" />
+    </div>
+  );
+};
+
+export default App;
+
+
+
